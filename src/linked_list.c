e4061e970b928d26096c7f35aa7b337457a4acc5
Chi Zu <chizu7993@gmail.com>
2025-09-30 06:48:44 -0400

    Defined `_POSIX_C_SOURCE` and added `Node__from` function
    Also added crucial `NULL` check in `Node_free` function.

--- src/linked_list.c
+++ src/linked_list.c
@@ -1,6 +1,8 @@
 #ifndef C_LINKED_LIST_H
 #define C_LINKED_LIST_H
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -59,10 +61,8 @@ int8_t Node__init(Node * *const nodeptr_addr, void *data, Node *next) {
 	if (nodeptr_addr == NULL) return -1; // No Node to operate on
 
 	// If there's no Node...
-	if (*nodeptr_addr == NULL) { // Make new Node on heap
-		if (Node__new(nodeptr_addr, data, next) == 1) return 1; // Node__new's malloc failed
-		return 0;
-	}
+	if (*nodeptr_addr == NULL) // Make a new Node on the heap
+		return Node__new(nodeptr_addr, data, next); // returns 0 on success or 1 if malloc fails
 
 	// If there is a Node, then assume it's on the stack
 	(*nodeptr_addr)->data = data;
@@ -73,14 +73,28 @@ int8_t Node__init(Node * *const nodeptr_addr, void *data, Node *next) {
 }
 
 /**
+ * @brief A shorter and simpler function for creating a `Node` in memory.
+ * @param data The data to point to
+ * @param next The next `Node` struct to point to
+ * @return A `Node *` or NULL if malloc fails
+ * @note This function uses `Node__new`.
+ */
+Node *Node__from(void *data, Node *next) {
+	Node *ret = NULL; // Set up for Node__new
+	Node__new(&ret, data, next); // Node__new ends with ret either being an actual allocated Node or NULL
+	return ret; // Return a Node or NULL
+}
+
+/**
  * @brief Free a `Node` from the heap.
  * @param nodeptr_addr The address of a `Node *`
- * @return -1 if `nodeptr_addr == NULL`, 0 on success, and 1 if the Node's `in_memory` member holds `false`.
+ * @return -1 if `nodeptr_addr == NULL`, 0 on success, and 1 if the actual `Node *` is `NULL`, and 2 if the Node's `in_memory` member holds `false`.
  */
 int8_t Node__free(Node * *const nodeptr_addr) {
 	if (nodeptr_addr == NULL) return -1; // No Node to free
 
-	if (!((*nodeptr_addr)->in_memory)) return 1; // Node is not on the heap
+	if (*nodeptr_addr == NULL) return 1; // Check if the actual Node object is NULL before accessing it
+	if (!((*nodeptr_addr)->in_memory)) return 2; // Node is not on the heap
 
 	free(*nodeptr_addr); // Free the Node
 	*nodeptr_addr = NULL; // Prevent dangling pointer
@@ -114,10 +128,10 @@ int8_t Node__free_nexts(Node * *const nodeptr_addr) {
  * @return -1 if `nodeptr_addr == NULL` or 0 on success.
  */
 int8_t Node__free_now_and_nexts(Node * *const nodeptr_addr) {
-	if (nodeptr_addr == NULL) return -1;
+	if (nodeptr_addr == NULL) return -1; // No Node to operate on
 
-	Node__free_nexts(nodeptr_addr);
-	Node__free(nodeptr_addr);
+	Node__free_nexts(nodeptr_addr); // Call Node__free_nexts to free consecutive Nodes and to store the current to also free it
+	Node__free(nodeptr_addr); // Free the current Node
 
 	return 0;
 }
